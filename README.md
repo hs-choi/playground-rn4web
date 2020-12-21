08583350ddba0313e071662fc64b934207f7f8ab
NohSeungWon1 <sw3noh@cartool.co>
2020-12-21 11:56:59 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. npx create
+2. cd folder
+3. npm install react-native-web
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  app: {
+    height: '100%'
+  },
+  appText: {
+    margin: 'auto'
+  }
+});
+
+const App = () => {
+  return (
+    <View style={styles.app}>
+      <Text style={styles.appText}>hello world!</Text>
+    </View>
+  );
+};
+export default App;
